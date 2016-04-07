38564113b71dca276fd16f30bba7ea9613bbcfaa
lijiang <lijiang@honglingjin.xyz>
2016-04-07 16:11:56 +0800

    Initial Commit

---
+++ RSUser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSUser
+//
+//  Created by 李江 on 16/4/7.
+//  Copyright © 2016年 RedScarf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
